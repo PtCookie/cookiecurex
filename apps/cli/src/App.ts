604ed57d9038d5b40f4bdf9d7e45bc756f86c63b
PtCookie <ptcookie@outlook.com>
2022-01-17 01:10:55 +0900

    fix: Handle Promise rejection
    Add try-catch to handle Promise rejection.

--- apps/cli/src/App.ts
+++ apps/cli/src/App.ts
@@ -13,30 +13,45 @@ class App {
   }
 
   async initialize(): Promise<void> {
-    const packageJson = JSON.parse((await readFile('./package.json')).toString());
+    try {
+      const packageJson = JSON.parse((await readFile('./package.json')).toString());
 
-    this.#program
-      .version(packageJson.version)
-      .option('-f, --from <currency>', 'exchange currency from...', 'krw')
-      .option('-t, --to <currency>', 'exchange currency to...', 'usd')
-      .option('-d, --date <date>', 'exchange currency of specific date.', 'latest')
-      .requiredOption('-a --amount <amount>', 'exchange amount.');
+      this.#program
+        .version(packageJson.version)
+        .option('-f, --from <currency>', 'exchange currency from...', 'krw')
+        .option('-t, --to <currency>', 'exchange currency to...', 'usd')
+        .option('-d, --date <date>', 'exchange currency of specific date.', 'latest')
+        .requiredOption('-a --amount <amount>', 'exchange amount.');
 
-    this.#program.parse();
+      this.#program.parse();
+    } catch (error: unknown) {
+      console.log(`${chalk.red("Can't parse version info.")}`);
+      console.log(`${chalk.green((error as Error).toString())}`);
+
+      process.exit(1);
+    }
   }
 
   async execute(): Promise<void> {
-    const { from, to, date, amount }: { from: string; to: string; date: string; amount: number } = this.#program.opts();
-    const result = (await exchange(amount, from, to, date)) as { rate: number; amount: number };
+    try {
+      const { from, to, date, amount }: { from: string; to: string; date: string; amount: number } =
+        this.#program.opts();
+      const result = (await exchange(amount, from, to, date)) as { rate: number; amount: number };
 
-    console.log(`
+      console.log(`
       ${chalk.red(from)} to ${chalk.red(to)} at ${chalk.blue(
-      date === 'latest' ? format(new Date(), 'yyyy-MM-dd') : date,
-    )} [Rate : ${chalk.magenta(result.rate)}] :
+        date === 'latest' ? format(new Date(), 'yyyy-MM-dd') : date,
+      )} [Rate : ${chalk.magenta(result.rate)}] :
     
       ${from} : ${chalk.green(amount)}
       ${to} : ${chalk.green(Math.round(result.amount * 1000) / 1000)}
     `);
+    } catch (error: unknown) {
+      console.log(`${chalk.red("Can't fetch exchange API.")}`);
+      console.log(`${chalk.green((error as Error).toString())}`);
+
+      process.exit(1);
+    }
   }
 }
 
--- apps/cli/src/curex.ts
+++ apps/cli/src/curex.ts
@@ -1,10 +1,25 @@
 import App from './App.js';
+import chalk from 'chalk';
 
 // Create App instance
 const app = new App();
 
 // Initialize app's metadata
-await app.initialize();
+try {
+  await app.initialize();
+} catch (error: unknown) {
+  console.log(`${chalk.red('Application initialize with error.')}`);
+  console.log(`${chalk.green((error as Error).toString())}`);
 
-// Execute app
-await app.execute();
+  process.exit(1);
+} finally {
+  try {
+    // Execute app
+    await app.execute();
+  } catch (error: unknown) {
+    console.log(`${chalk.red('Application execute with error.')}`);
+    console.log(`${chalk.green((error as Error).toString())}`);
+
+    process.exit(1);
+  }
+}
